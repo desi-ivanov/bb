08391dfb42ef7427747022bb844f18c66b887850
Desislav Ivanov <20824840+EvolveYourMind@users.noreply.github.com>
2022-02-19 11:15:47 +0100

    Improve navigation

--- packages/web/src/components/Playground.tsx
+++ packages/web/src/components/Playground.tsx
@@ -4,12 +4,11 @@ import { SVGNode } from "./SVGNode";
 import { depth } from "./utils";
 
 export const Playground: React.FC<{ root: BBNode }> = React.memo(({ root }) => {
-  const [zoom, setZoom] = useState(1)
+  const [xSpacingMultiplier, setXSpacingMultiplier] = useState(1)
   const rootDivRef = useRef<HTMLDivElement | null>(null);
-  const mouseState = useRef<{
-    mouseDown: boolean;
-    lastPos: null | { x: number; y: number };
-  }>({ mouseDown: false, lastPos: null });
+  const mouseState = useRef<{ mouseDown: boolean; lastPos: null | { x: number; y: number }; }>({ mouseDown: false, lastPos: null });
+  const [t, setT] = useState({ x: 0, y: 0 });
+  const [z, setZ] = useState(1);
 
   const handleMouseMove: React.MouseEventHandler<HTMLDivElement> = useCallback(
     (e) => {
@@ -20,8 +19,7 @@ export const Playground: React.FC<{ root: BBNode }> = React.memo(({ root }) => {
       ) {
         const dx = e.clientX - mouseState.current.lastPos.x;
         const dy = e.clientY - mouseState.current.lastPos.y;
-        rootDivRef.current.scrollTop -= dy;
-        rootDivRef.current.scrollLeft -= dx;
+        setT(pt => ({ x: pt.x + dx, y: pt.y + dy }));
       }
       mouseState.current.lastPos = { x: e.clientX, y: e.clientY };
     },
@@ -46,34 +44,28 @@ export const Playground: React.FC<{ root: BBNode }> = React.memo(({ root }) => {
     []
   );
 
+  const handleWheel: React.WheelEventHandler<HTMLDivElement> = useCallback(e => setZ(pz => pz - Math.sign(e.deltaY) * 0.1), []);
+
   const d = depth(root);
   const ySpacing = 150;
-  const xSpacing = 110 * zoom;
+  const xSpacing = 110 * xSpacingMultiplier;
   return (
-    <div
-      style={{
-        flex: 1,
-        display: "flex",
-        flexDirection: 'column'
-      }}
-    >
-      <input type="range" min={0.1} max={2} step={0.01} onChange={e => setZoom(Number(e.target.value))} style={{ margin: 16, width: 200 }} />
+    <div style={{ flex: 1, display: "flex", flexDirection: 'column' }}>
+      <input type="range" min={0.1} max={2} step={0.01} onChange={e => setXSpacingMultiplier(Number(e.target.value))} style={{ margin: 16, width: 200 }} />
       <div
         ref={rootDivRef}
-        style={{
-          overflow: "hidden",
-          flex: 1,
-        }}
+        style={{ overflow: "hidden", flex: 1 }}
         onMouseDown={handleMouseDown}
         onMouseUp={handleMouseUp}
         onMouseMove={handleMouseMove}
         onMouseLeave={handleMouseUp}
+        onWheel={handleWheel}
       >
         <svg
           viewBox={`-75 -75 10000 10000`}
           width={10000}
           height={10000}
-          style={{ position: "relative" }}
+          style={{ position: "relative", transform: `translate(${t.x}px,${t.y}px )` }}
           xmlns="http://www.w3.org/2000/svg"
         >
           <SVGNode
@@ -82,7 +74,7 @@ export const Playground: React.FC<{ root: BBNode }> = React.memo(({ root }) => {
             startY={0}
             ySpacing={ySpacing}
             dedicatedWidth={xSpacing * (2 ** (d - 1))}
-            zoom={1}
+            zoom={z}
           />
         </svg>
       </div>
